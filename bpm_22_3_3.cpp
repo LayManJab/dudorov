7529c4e080eb5b7c2cd0f32250ac7ed9e37ae37b
LayManJab <113929774+LayManJab@users.noreply.github.com>
2022-12-26 19:59:39 +0300

    Update bpm_22_3_3.cpp

--- bpm_22_3_3.cpp
+++ bpm_22_3_3.cpp
@@ -1,30 +1,32 @@
-#include"iostream"
-#include"string"
-#include"vector"
+#include <iostream>
+#include <string>
+#include <vector>
 
 int main() {
-	int a;
-	string b;
-	std::cin << a;
-	vector <string> vec;
-	for (int i = 0; i < a; i++)
-	{
-		std::cin << b;
-		vec.push_back(b)
-	}
 
-	for (int i = 0; i < a; i++)
-	{
-		if (vec[i] == vec[i + 1] == vec[i + 2]) {
-			std::cout >> "YES"
-		}
-		else
-		{
-			std::cout >> "NO"
-		}
-	}
+    int n, sum=0;
+    std::vector <std::string> vec;
+    std::string a;
+    std::cin >> n;
+
+    for(int i=0; i<n; i++){
+        std::cin >> a;
+        vec.push_back(a);
+    }
+
+    for(int i=0; i<n; i++){
+        if(vec[i] == vec[i+1] and vec[i+1] == vec[i+2]){
+            sum++;
+        }
+    }
+
+    if(sum > 0){
+        std::cout << "YES";
+    }
+    else{
+        std::cout << "NO";
+    }
 
 
-
-	return 0;
+    return 0;
 }
